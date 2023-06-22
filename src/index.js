b3e125d8e5a83ef44f2a359fe02f2d2ddd7d33bf
deveshdevlife <webdeveshverma@gmail.com>
2023-06-22 14:49:46 +0530

    fix confict issue

--- src/index.js
+++ src/index.js
@@ -4,15 +4,22 @@ const  cors= require("cors");
 const path=require('path')
 const app= express();
 const port=process.env.PORT || 4568;
+//** Import controllers
+const {register,login}= require("./controller/Auth/auth.controller");
+const userController= require("./controller/User/user.controller");
+const blogController= require("./controller/blog.controller");
 
-//!global middlewares 
+//!global middlewares
 app.use(cors());
 app.use(express.json())
+//** defining routes */
+//?auth routes
+app.post("/api/register",register)
+app.post("/api/login",login)
+//?user routes
+app.use("/api/users",userController)
 app.use("/blogs",blogController)
-
-
-
-//!listen to the server 
+//!listen to the server
 app.listen( port  , async()=>{
     try {
           await connect()
