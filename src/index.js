76bfe031d1ead52cb875c80d4209aef80c37288e
iqbal.webpatriot <iqbal.webpatriot@gmail.com>
2023-06-22 11:33:05 +0530

    initial setup

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express= require("express");
+const connect= require("./configs/db");
+const  cors= require("cors");
+const path=require('path')
+const app= express();
+const port=process.env.PORT || 4568;
+
+//!global middlewares 
+app.use(cors());
+app.use(express.json())
+
+//!listen to the server 
+app.listen( port  , async()=>{
+    try {
+          await connect()
+          console.log(`listening port number ${port}   `)
+    } catch (error) {
+        console.log("Error while connecting to database" , error.message)
+    }
+})
